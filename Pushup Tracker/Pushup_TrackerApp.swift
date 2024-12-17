22ed1b50c44baa0e26e5a6150606d5fe365e474b
maddiehutch1 <madhutch@byu.edu>
2024-12-17 16:08:59 -0700

    Initial Commit

---
+++ Pushup Tracker/Pushup_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pushup_TrackerApp.swift
+//  Pushup Tracker
+//
+//  Created by IS 543 on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Pushup_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
